947c473395f9e5acf9acc1b5e8343bbb765b0acf
long <271858485@qq.com>
2018-06-24 21:00:58 +0800

    update

--- canvas.js
+++ canvas.js
@@ -44,3 +44,50 @@ for (i=0;i<8;i++) {
 ctx.lineCap = "butt";//线段末端以方形结束
 ctx.lineCap = "round";//线段末端以圆形结束
 ctx.lineCap = "square";//线段末端以方形结束，但是增加了一个宽度和线段相同，高度是线段厚度一半的矩形区域。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var cvs = document.getElementById('cvs');  
+var ctx = cvs.getContext('2d');  
+var img = document.querySelector('img');  
+img.onload = function() {  
+	var i = 0;  
+	setInterval( function() {  
+		// 绘制新的图像时，需要先清除画布  
+		ctx.clearRect( 0, 0, cvs.width, cvs.height );  
+
+		// 绘图每一帧  
+		/* 
+		* 裁剪的x轴，用来控制每一排不同的帧， 
+		* 裁剪的y轴，用来控制当前行走的方向(也就是绘制那一排)。 
+		* */  
+		ctx.drawImage( img,  
+			img.width / 4 * i, img.height / 4 * 0, img.width / 4, img.height / 4,  
+			100, 10, 50, 60 );  
+
+		// i的最大值为3  
+		if ( ++i >= 4 ) {  
+			i = 0;  
+		}  
+	}, 200);  
+};  
